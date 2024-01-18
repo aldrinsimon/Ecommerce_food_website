29e9780e93250388085f66435cb18da596790043
Your Name <you@example.com>
2024-01-18 14:15:27 +0530

    initial commit

---
+++ veg/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!D:\ECommerceSite-Django (2)\ECommerceSite-Django\veg\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
